e09783bc10bfacbb01b6bfa0e0ace33ec3101817
Devina <devinardya@gmail.com>
2020-03-05 11:43:44 +0100

    Updates

---
+++ precache-manifest.1a47ae17567cb913671704cae470957c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe5f575f706404357db684650d0ba44",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "c4e2745f7680124b1e13",
+    "url": "/movie-app/static/css/main.f3fccd58.chunk.css"
+  },
+  {
+    "revision": "d2524583e540030ed24e",
+    "url": "/movie-app/static/js/2.0a6ccdda.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie-app/static/js/2.0a6ccdda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4e2745f7680124b1e13",
+    "url": "/movie-app/static/js/main.ce6e57de.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
