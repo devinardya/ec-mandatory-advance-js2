9f937952a99bd4ca904d7b1e2526e6f9eca7c349
Devina <devinardya@gmail.com>
2020-03-09 12:00:55 +0100

    Updates

--- precache-manifest.f94ec480f21dbdef9df3ba33781d0dc1.js
+++ precache-manifest.f94ec480f21dbdef9df3ba33781d0dc1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a45f9733f475cc82125906debcef8b17",
+    "revision": "d5a63da4881358d3c3ca1128e1f6862a",
     "url": "/ec-mandatory-advance-js2/index.html"
   },
   {
-    "revision": "5ca2601d1e8864f85d87",
+    "revision": "cac15539e2841cd965a8",
     "url": "/ec-mandatory-advance-js2/static/css/main.f3fccd58.chunk.css"
   },
   {
-    "revision": "d2524583e540030ed24e",
-    "url": "/ec-mandatory-advance-js2/static/js/2.0a6ccdda.chunk.js"
+    "revision": "118e6573182b901bfe04",
+    "url": "/ec-mandatory-advance-js2/static/js/2.26ada5cd.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/ec-mandatory-advance-js2/static/js/2.0a6ccdda.chunk.js.LICENSE"
+    "url": "/ec-mandatory-advance-js2/static/js/2.26ada5cd.chunk.js.LICENSE"
   },
   {
-    "revision": "5ca2601d1e8864f85d87",
-    "url": "/ec-mandatory-advance-js2/static/js/main.19641891.chunk.js"
+    "revision": "cac15539e2841cd965a8",
+    "url": "/ec-mandatory-advance-js2/static/js/main.9b8c0c85.chunk.js"
   },
   {
     "revision": "125b19874a2b0f0fef0e",
