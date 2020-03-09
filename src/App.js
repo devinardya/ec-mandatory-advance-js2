a8fbc4abe8444776b2714fba95bc8f616d59e73a
Devina <devinardya@gmail.com>
2020-03-09 11:58:56 +0100

    using hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, HashRouter} from 'react-router-dom';
 import Home from './MoviesDirectory';
 import AddMovie from './AddMovie';
 import EditMovie from './EditMovie';
@@ -10,12 +10,12 @@ class App extends React.Component {
   render(){
     return (
       <div id="App">
-          <Router basename="/">
+          <HashRouter basename="/">
               <Route exact path="/" component={Home} />
               <Route path="/addmovie" component={AddMovie} />
               <Route path="/editmovie/:id" component={EditMovie} />
               <Route path="/movies/:id" component={IndividualMovie} />
-           </Router>
+           </HashRouter>
         
       </div>
     );
